1f98d72a78d7188b58d71602bb3507234f54d081
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-08-16 15:34:39 -0500

    Get some results from website

--- BexarLandMine/__main__.py
+++ BexarLandMine/__main__.py
@@ -1,5 +1,7 @@
-from requests import get
+from requests import post
 from BexarLandMine.Config import Config
 
+data = "searchby=5&criteria=000000"
 conf = Config("config.toml")
-print(get(conf.data["bexar_index_url"]).text)
+res = post(conf.data["bexar_list_url"], headers=conf.data["headers"], data=data)
+print(res.text)
