e76eb13bc0d47581d4814e3c79b5ad36861e8d54
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-08-18 17:56:59 -0500

    Add html parser module

--- BexarLandMine/__main__.py
+++ BexarLandMine/__main__.py
@@ -1,7 +1,9 @@
 from requests import post
 from BexarLandMine.Config import Config
+from BexarLandMine.Parser import MyHTMLParser
 
 data = "searchby=5&criteria=000000"
 conf = Config("config.toml")
+p = MyHTMLParser()
 res = post(conf.data["bexar_list_url"], headers=conf.data["headers"], data=data)
-print(res.text)
+p.feed(res.text)
