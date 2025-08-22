d39b5c62518cfc80cf9e83a722b8682d5462b5b7
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-08-22 17:25:17 -0500

    Use submodules

--- BexarLandMine/HTML/Node/__init__.py
+++ BexarLandMine/HTML/Node/__init__.py
@@ -1,1 +1,7 @@
+class HTMLNode():
+    def __init__(self, tag: str):
+        self.tag = tag
+        self.children = []
 
+    def __repr__(self):
+        return "HTMLNode()"
--- BexarLandMine/__main__.py
+++ BexarLandMine/__main__.py
@@ -1,9 +1,10 @@
 from requests import post
 from BexarLandMine.Config import Config
-from BexarLandMine.Parser import MyHTMLParser
+from BexarLandMine.HTML.Parser import ResultListHTMLParser
 
 data = "searchby=5&criteria=000000"
 conf = Config("config.toml")
-p = MyHTMLParser()
-res = post(conf.data["bexar_list_url"], headers=conf.data["headers"], data=data)
+p = ResultListHTMLParser()
+res = post(conf.data["bexar_list_url"],
+           headers=conf.data["headers"], data=data)
 p.feed(res.text)
