c1d6dc85743ed3f98c3b0aef945f03d9fa1e4c70
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-08-26 14:40:29 -0500

    Get detail pages directly
    It turns out that the pages can be directly queries by using the account
    number in the URL.

--- BexarLandMine/__main__.py
+++ BexarLandMine/__main__.py
@@ -2,9 +2,11 @@ from requests import post
 from BexarLandMine.Config import Config
 from BexarLandMine.HTML.Parser import ResultListHTMLParser
 
-data = "searchby=5&criteria=000000"
+account_number = 1
+data = f"can={account_number:012}"
 conf = Config("config.toml")
 p = ResultListHTMLParser()
-res = post(conf.data["bexar_list_url"],
-           headers=conf.data["headers"], data=data)
+url = f"{conf.data["bexar_details_url"]}?{data}"
+print(f"Getting: {url}")
+res = post(url, headers=conf.data["headers"], data=data)
 p.feed(res.text)
