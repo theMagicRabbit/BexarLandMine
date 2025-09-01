6b414ddfaee3ea104f6020d2f6e611b29313ab93
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-09-01 16:55:05 -0500

    Add tests

--- tests/test_details.py
+++ tests/test_details.py
@@ -1,5 +1,5 @@
 from BexarLandMine.HTML.Node import HTMLNode
-from BexarLandMine.Land import filter_account_num
+from BexarLandMine.Land import filter_account_num, Detail, detail_from_html_node
 
 
 def test_filter_account_num():
@@ -38,3 +38,22 @@ def test_filter_account_num():
     for input, expected in tests:
         result = filter_account_num(input)
         assert result == expected
+
+
+def test_details():
+    tests = [
+        (
+            HTMLNode(tag='div', value=None, children=[
+                HTMLNode(tag='label', value=None, children=[
+                    HTMLNode(tag=None, value='Account Number:', children=[])
+                ]),
+                HTMLNode(tag=None, value='000000000001', children=[])
+            ]),
+            (Detail(1), '000000000001')
+        ),
+    ]
+    for input, expected in tests:
+        node, can = expected
+        result = detail_from_html_node(input)
+        assert result == node
+        assert result.account_number() == can
