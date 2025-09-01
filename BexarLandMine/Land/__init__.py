63e6a8833c41c4833190f43759974493e6c9f5b4
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-09-01 16:16:48 -0500

    Export filter function for testing

--- BexarLandMine/Land/__init__.py
+++ BexarLandMine/Land/__init__.py
@@ -1,7 +1,8 @@
-from .detail import Detail, detail_from_html_node
+from .detail import Detail, detail_from_html_node, filter_account_num
 
 
 __all__ = [
+        "filter_account_num",
         "Detail",
         "detail_from_html_node",
         ]
