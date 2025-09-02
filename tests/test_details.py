a3b640b94230482982863e08fe68887db5df9381
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-09-02 10:37:57 -0500

    Update tests for new filter pattern

--- tests/test_details.py
+++ tests/test_details.py
@@ -1,42 +1,93 @@
 from BexarLandMine.HTML.Node import HTMLNode
-from BexarLandMine.Land import filter_account_num, Detail, detail_from_html_node
+from BexarLandMine.Land import filter_detail_table, Detail, detail_from_html_node
 
 
-def test_filter_account_num():
+def test_filters():
+    node_filters = [
+            (
+                'account_number_node',
+            ),
+            (
+                'account_address_node',
+                'Address:'
+            ),
+            (
+                'property_address_node',
+                'Property Site Address:'
+            ),
+        ]
     tests = [
         (
-            HTMLNode(tag='div', value=None, children=[
-                HTMLNode(tag='label', value=None, children=[
-                    HTMLNode(tag=None, value='Account Number:', children=[])
+            (
+                HTMLNode(tag='div', value=None, children=[
+                    HTMLNode(tag='label', value=None, children=[
+                        HTMLNode(tag=None, value='Account Number:',
+                                 children=[])
+                    ]),
+                    HTMLNode(tag=None, value='000000000001', children=[])
                 ]),
-                HTMLNode(tag=None, value='000000000001', children=[])
-            ]),
+                'Account Number:'
+            ),
             True
         ),
         (
-            HTMLNode(tag='div', value=None, children=[
-                HTMLNode(tag='label', value=None, children=[
-                    HTMLNode(tag=None, value='Address:', children=[])
+            (
+                HTMLNode(tag='div', value=None, children=[
+                    HTMLNode(tag='label', value=None, children=[
+                        HTMLNode(tag=None, value='Address:', children=[])
+                    ]),
+                    HTMLNode(tag=None, value='000000000001', children=[])
                 ]),
-                HTMLNode(tag=None, value='Test Account', children=[])
-            ]),
+                'Account Number:'
+            ),
             False
         ),
         (
-            HTMLNode(tag='div', value=None, children=[
-                HTMLNode(tag='label', value=None, children=[
+            (
+                HTMLNode(tag='div', value=None, children=[
+                    HTMLNode(tag='label', value=None, children=[
+                        HTMLNode(tag=None, value='Address:', children=[])
+                    ]),
+                    HTMLNode(tag=None, value='000000000001', children=[])
                 ]),
-                HTMLNode(tag=None, value='Test Account', children=[])
-            ]),
+                'Account Number:'
+            ),
             False
         ),
         (
-            HTMLNode(tag='div', value=None, children=[]),
+            (
+                HTMLNode(tag='div', value=None, children=[
+                    HTMLNode(tag='label', value=None, children=[
+                        HTMLNode(tag=None, value='Address:', children=[])
+                    ]),
+                ]),
+                'Address:'
+            ),
             False
+        ),
+        (
+            (
+                HTMLNode(tag='div', value=None, children=[]),
+                'Address:'
+            ),
+            False
+        ),
+        (
+            (
+                HTMLNode(tag='div', value=None, children=[
+                    HTMLNode(tag='label', value=None, children=[
+                        HTMLNode(tag=None, value='Address:',
+                                 children=[])
+                    ]),
+                    HTMLNode(tag=None, value='TEST ACCOUNT', children=[])
+                ]),
+                'Address:'
+            ),
+            True
         ),
     ]
     for input, expected in tests:
-        result = filter_account_num(input)
+        result = filter_detail_table(*input)
         assert result == expected
 
 
